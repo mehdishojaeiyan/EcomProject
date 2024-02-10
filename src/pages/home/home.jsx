d92c3bda6bd5b27c33f4890a00b41efd70ffe18d
unknown <mehdishojaeiyan96@gmail.com>
2024-02-10 17:24:03 +0330

    start home page

--- src/pages/home/home.jsx
+++ src/pages/home/home.jsx
@@ -1,42 +1,28 @@
-import "./home.css"
-import React from 'react';
-import { Line } from 'react-chartjs-2';
-import { Chart, ArcElement } from 'chart.js';
-Chart.register(ArcElement);
+import "./home.css";
+import React from "react";
+import homeTrade from "../../assets/image/homeTrade2.jpeg";
 
-
-const data = {
-    labels: ['January', 'February', 'March', 'April', 'May', 'June'],
-    datasets: [
-      {
-        label: 'My First Dataset',
-        data: [65, 59, 80, 81, 56, 55],
-        fill: false,
-        borderColor: 'rgb(75, 192, 192)',
-        tension: 0.1,
-      },
-    ],
-  };
-  
-  const options = {
-    scales: {
-      y: {
-        beginAtZero: true,
-      },
-    },
-  };
-
-  
 const Home = () => {
-    return ( <>
-    <div className="homeContainer">
+  return (
+    <>
+      <div className="homeContainer ">
+        <div className="headHome mt-3 mb-3">
+          <div className="sloganDivImg">
+            <img className="sloganImg" src={homeTrade} alt="" />
+          </div>
+          <div className="sloganH1">
+            {" "}
+            <h2>
+              “Smart Trading Starts Here!”
+              <h2 className="mt-5">
+                “Choose Your Strategy, Trade Like a Pro!”
+              </h2>
+            </h2>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
 
-    <div>
-    <h2>My Awesome Chart</h2>
-    {/* <Line data={data} options={options} /> */}
-  </div>
-    </div>
-    </> );
-}
- 
 export default Home;
