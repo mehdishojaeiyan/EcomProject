24488b57cbd6244041b442a6b1fdcda3fd7e33b3
unknown <mehdishojaeiyan96@gmail.com>
2024-03-08 10:57:30 -0800

    added all forex table

--- src/components/allForex/allForex.jsx
+++ src/components/allForex/allForex.jsx
@@ -63,8 +63,18 @@
 
 
 
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect ,useRef} from 'react';
 import axios from 'axios';
+import { styled } from '@mui/material/styles';
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell, { tableCellClasses } from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
+import Paper from '@mui/material/Paper';
+import DigiCoinLoading from '../digicoin/digiCoinLoading';
+import CryptoChart from '../../pages/market/crypto/CryptoChart';
 import './allForex.css';
 
 const AllForex = () => {
@@ -108,15 +118,67 @@ const AllForex = () => {
 
   // Split exchange rates into two arrays for two columns
   const exchangeRatesArray = Object.entries(exchangeRates.rates);
-  const halfLength = Math.ceil(exchangeRatesArray.length / 2);
-  const firstColumn = exchangeRatesArray.slice(0, halfLength);
-  const secondColumn = exchangeRatesArray.slice(halfLength);
+ 
 
+
+
+  
+  const StyledTableCell = styled(TableCell)(({ theme }) => ({
+    [`&.${tableCellClasses.head}`]: {
+      backgroundColor: theme.palette.common.black,
+      color: theme.palette.common.white,
+    },
+    [`&.${tableCellClasses.body}`]: {
+      fontSize: 14,
+    },
+  }));
+  
+  const StyledTableRow = styled(TableRow)(({ theme }) => ({
+    '&:nth-of-type(odd)': {
+      backgroundColor: theme.palette.action.hover,
+    },
+    // hide last border
+    '&:last-child td, &:last-child th': {
+      border: 0,
+    },
+  }));
   return (
-    <div>
+    <div className='p-5'>
       <h1 className='mt-4 p-4'>Forex Exchange Rate</h1>
+      
+            {loading ? (
+                <DigiCoinLoading/>
+            ) : (
+                <TableContainer component={Paper}>
+                <Table sx={{ minWidth: 700 }} aria-label="customized table">
+                  <TableHead>
+                    <TableRow>
 
-      <div className='allForex p-5'>
+                      <StyledTableCell>N.O</StyledTableCell>
+                      <StyledTableCell>Name</StyledTableCell>
+                      <StyledTableCell align="right">Symbol</StyledTableCell>
+                      <StyledTableCell align="right">Current Price (USD)</StyledTableCell>
+                      
+                      <StyledTableCell align="right">Chart</StyledTableCell>
+                    </TableRow>
+                  </TableHead>
+                  <TableBody>
+                        {exchangeRatesArray.map(([currency, rate], index) => (
+                            <StyledTableRow key={currency}>
+                                <StyledTableCell component="th" scope="row">{index+1}</StyledTableCell>
+                                <StyledTableCell component="th" scope="row"> {exchangeRates.names[currency]}</StyledTableCell>
+                                <StyledTableCell align="right">({currency})/USD</StyledTableCell>
+                                <StyledTableCell align="right">{rate}</StyledTableCell>
+                                
+                                <StyledTableCell align="right"><CryptoChart/></StyledTableCell>
+                            </StyledTableRow>
+                        ))}
+                     </TableBody>
+      </Table>
+    </TableContainer>
+            )}
+
+      {/* <div className='allForex p-5'>
         <div className='forexColumn p-5'>
           {firstColumn.map(([currency, rate], index) => (
             <div className='allMarketForex p-3' key={currency}>
@@ -133,9 +195,10 @@ const AllForex = () => {
             </div>
           ))}
         </div>
-      </div>
+      </div> */}
     </div>
   );
 };
+
 
 export default AllForex;
